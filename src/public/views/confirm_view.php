8c8664ce34ab7fd062f071ba44bc524d60cc97ca
shuntaro-fukuta <shuntaro.fkt@gmail.com>
2021-01-17 01:30:14 +0900

    2重送信を防止するため、送信ボタンを押下後にJSでボタンをdisableにする

--- src/public/views/confirm_view.php
+++ src/public/views/confirm_view.php
@@ -5,7 +5,7 @@
   <title>確認画面</title>
 </head>
 <body>
-  <form action="register.php" method="post">
+  <form id="confirm_form" action="register.php" method="post">
     <p>件名：<?= h(InquiryForm::SUBJECT[$inquiry_form->getSubject()]) ?></p>
     <p>名前：<?= h($inquiry_form->getName()) ?></p>
     <p>メールアドレス：<?= h($inquiry_form->getEmail()) ?></p>
@@ -15,8 +15,19 @@
       <?= nl2br(h($inquiry_form->getInquiry())) ?>
     </div>
 
-    <input type="button" value="修正" onclick="location.href='inquiry_form.php'">
-    <input type="submit" value="送信">
+    <input id="back_button" type="button" value="修正" onclick="location.href='inquiry_form.php'">
+    <input id="submit_button" type="button" value="送信">
   </form>
+
+  <script>
+    const submit_button = document.getElementById('submit_button');
+    const back_button = document.getElementById('back_button');
+    const confirm_form= document.getElementById('confirm_form');
+    submit_button.addEventListener('click', () => {
+      back_button.disabled = true;
+      submit_button.disabled = true;
+      confirm_form.submit();
+    });
+  </script>
 </body>
 </html>

1ea52ddd03ba5681b946ed78c49fc20acccacb22
shuntaro-fukuta <shuntaro.fkt@gmail.com>
2021-01-18 14:12:30 +0900

    ログファイルが存在しなかった場合、ログが出力されなかったので修正

--- config/log_config.php
+++ config/log_config.php
@@ -1,7 +1,11 @@
 <?php
 
 const LOG_DIR = APP_ROOT . DIR_SEP . 'log';
-
 const LOG_FILENAME = 'error.log';
+
+if (!file_exists(LOG_DIR . DIR_SEP . LOG_FILENAME)) {
+    touch(LOG_DIR . DIR_SEP . LOG_FILENAME);
+    chmod(LOG_DIR . DIR_SEP . LOG_FILENAME, 0755);
+}
 
 ini_set('error_log', LOG_DIR . DIR_SEP . LOG_FILENAME);
